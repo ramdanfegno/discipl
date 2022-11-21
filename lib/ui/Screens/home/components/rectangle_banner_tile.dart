994b83e385ac442c521ab52b0bdcd20beab4c639
ramdanfegno <108392413+ramdanfegno@users.noreply.github.com>
2022-11-21 15:37:02 +0530

    15:36 monday 21 nov

---
+++ lib/ui/Screens/home/components/rectangle_banner_tile.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+
+class GymsNearWidget extends StatelessWidget {
+  const GymsNearWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text('Popular ')
+          ],
+        )
+      ],
+    );
+  }
+}
